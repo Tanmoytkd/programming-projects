9224a4a14dce6ee40f6f9f85a7390d1647a01fa7
Tanmoytkd <tanmoykrishnadas@gmail.com>
2016-06-13 18:15:31 +0600

    Added PICK UP DROP ESCAPE

--- PICK UP DROP ESCAPE (DP)/main.c
+++ PICK UP DROP ESCAPE (DP)/main.c
@@ -1,29 +1,29 @@
-#include<bits/stdc++.h>
+#include<stdio.h>
 #define pf printf
 #define sc scanf
 #define pb push_back
-using namespace std;
 
-int n, k, maxi=INT_MIN;
+int n, k, maxi=0;
 int data[30];
-bool check[30];
+char check[30];
 int counter=0, res=0;
 
-void calc() {
+void calc(int p) {
+    int i, j;
 	if(counter==k) {
 		if(res>maxi) maxi=res;
 		return;
 	}
 
-	for(int i=0; i<n; i++) {
+	for(i=p; i<n; i++) {
 		if(!check[i]) {
-			check[i]=true;
+			check[i]=1;
 			res^=data[i];
 			counter++;
-			calc();
+			calc(i+1);
 			counter--;
 			res^=data[i];
-			for(int j=i+1; j<n; j++) check[j]=false;
+			for(j=i+1; j<n; j++) check[j]=0;
 		}
 	}
 }
@@ -32,14 +32,15 @@ int main() {
 	int t;
 	sc("%d", &t);
 	while(t--) {
+        int i;
 		sc("%d %d", &n, &k);
-		maxi=INT_MIN;
+		maxi=0;
 		res=0;
-		for(int i=0; i<n; i++) {
+		for(i=0; i<n; i++) {
 			sc("%d", &data[i]);
-			check[i]=false;
+			check[i]=0;
 		}
-		calc();
+		calc(0);
 		pf("%d\n", maxi);
 	}
 
