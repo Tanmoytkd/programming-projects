fb21639055141f401e6ab04ea5e30deee0e9eb19
Tanmoytkd <tanmoykrishnadas@gmail.com>
2016-10-12 09:54:45 +0600

    Added precode

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define pf printf
+#define sf scanf
+#define pb push_back
+#define Pb pop_back
+#define sf1(x) scanf("%d", &x)
+#define sf2(x,y) scanf("%d %d", &x, &y)
+#define sf3(x,y,z) scanf("%d %d %d", &x, &y, &z)
+#define pf1(x) printf("%d\n", x)
+#define PI acos(-1.00)
+using namespace std;
+
+int main() {
+
+    return 0;
+}
