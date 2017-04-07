fcf9ca1397de7feab3fe87d72ffbaf59e61933ee
Tanmoytkd <tanmoykrishnadas@gmail.com>
2017-04-07 17:23:51 +0600

    changed castle

--- USACO/The Castle/castle.cpp
+++ USACO/The Castle/castle.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int lint;
+typedef pair<int, int> pii;
 
 int data[55][55];
 int r, c;
@@ -19,7 +21,20 @@ int main() {
     }
 
     int comp=0;
-
+    for(int i=1; i<=r; i++) {
+    	for(int j=1; j<=c; j++) {
+    		if(!visit[i][j]) {
+    			cnt=0;
+    			dfs(i,j);
+    			while(!v.empty()) {
+    				pii x=v.back();
+    				cmp[x.first][x.second]=comp;
+    				total[x.first][x.second]=cnt;
+    			}
+    			comp++;
+    		}
+    	}
+    }
 
     return 0;
 }
