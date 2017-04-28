d7ba8d71af702358bf151c234978ea6eede212d0
tanmoytkd <tanmoykrishnadas@gmail.com>
2017-04-28 10:32:35 +0600

    Corrected SPOJ - MIKE AND MICE

--- Two Four Four One One Three Nine/e.cpp
+++ Two Four Four One One Three Nine/e.cpp
@@ -3,33 +3,37 @@ using namespace std;
 
 template <class T>
 struct dijkstra {
-    int n;
-    vector<vector<pair<int, T> > > adj;
+    long long n;
+    vector<vector<pair<long long, T> > > adj;
     vector<T> dist;
-    vector<int> parent;
+    vector<long long> parent, mark;
 
-    dijkstra(int n): n(n+1), adj(n+1){} //dist and parent will be initialized during calculation
+    dijkstra(long long n): n(n+1), adj(n+1){} //dist and parent will be initialized during calculation
 
-    void addEdge(int a, int b, T w) {
-        adj[a].push_back({b, w});
-        //adj[b].push_back({a, w});
+    void addEdge(long long a, long long b, T w) {
+        //adj[a].push_back({b, w});
+        adj[b].push_back({a, w});
     }
 
-    void findShortestPath(int src) {
-        priority_queue<pair<T,int>, vector<pair<T, int> >, greater<pair<T, int> > > q;
+    void findShortestPath(long long src) {
+        priority_queue<pair<T,long long>, vector<pair<T, long long> >, greater<pair<T, long long> > > q;
         dist = vector<T>(n, numeric_limits<T>::max());
-        parent = vector<int>(n, -1);
+        parent = vector<long long>(n, -1);
+        mark = std::vector<long long> (n, 0);
         dist[src]=0;
         q.push({0, src});
 
         while(!q.empty()) {
-            int u  = q.top().second;
+            long long u  = q.top().second;
+            mark[u]=1;
             q.pop();
             //T d = q.top().first;
 
             for(auto edge: adj[u]) {
-                int v= edge.first;
+                long long v= edge.first;
                 T d = edge.second;
+
+                if(mark[v]) continue;
 
                 if(dist[v]-d > dist[u]) {
                     dist[v]=d+dist[u];
@@ -40,12 +44,12 @@ struct dijkstra {
         }
     }
 
-    T shortest_distance(int node) {
+    T shortest_distance(long long node) {
         return dist[node];
     }
 
-    vector<int> shortest_path(int node) {
-        vector<int> res;
+    vector<long long> shortest_path(long long node) {
+        vector<long long> res;
         while(node!=-1) {
             res.push_back(node);
             node=parent[node];
@@ -56,23 +60,23 @@ struct dijkstra {
 };
 
 int main() {
-    int n, s, t;
+    long long n, s, t;
     cin >> n >> s >> t;
-    dijkstra<int> d(n);
-    int m;
+    dijkstra<long long> d(n);
+    long long m;
     cin >> m;
     while(m--) {
-        int a, b, cost;
+        long long a, b, cost;
         cin >> a >> b >> cost;
         d.addEdge(a,b,cost);
     }
 
-    int cnt=0;
+    long long cnt=0;
     d.findShortestPath(s);
-    vector<int> & dist=d.dist;
-    for(int i=1; i<=n; i++) {
+    vector<long long> & dist=d.dist;
+    for(long long i=1; i<=n; i++) {
 
-        if(dist[i]<t) cnt++;
+        if(dist[i]<=t) cnt++;
     }
     cout << cnt << endl;
     return 0;
